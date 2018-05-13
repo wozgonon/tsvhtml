4428009fa9d927187ac9dc5fe39670b9289f482f
U-fabulord\jb <wozgonon@gmail.com>
2018-05-13 12:05:44 +0100

    Now uses CSS to make table borders look better, added comments, some code improvements

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,23 @@ use std::io;
 use std::io::Read;
 use std::io::Write;
 
+///
+///  A program that does nothing more than convert a TSV file into an HTML table.
+///  The first line of the TSV file is treated as the header.
+///
 fn main() {
 
-    let delimiter: char = '\t';
-
-    let out : HTML = HTML {};
-    out.tag("head");
-    out.tag("/head");
+    let out : HTMLWriter = HTMLWriter {};
+    out.element("head", "");
     out.tag("body");
-    out.write  ("<table border=\"1\">");
+    out.element("style", "table {  border-collapse: collapse; } td,th { border: 1px solid #999; }");
+    out.tag("table");
 
     let mut prefix = "<tr><th>";
-    let mut infix = "</th><th>";
+    let mut infix  = "</th><th>";
     let mut suffix = "</th></tr>";
 
-    let mut write_prefix = true;
+    let mut is_start_of_line = true;
     let mut character = [0];
     loop {
         match io::stdin().read(&mut character) {
@@ -25,43 +27,54 @@ fn main() {
                     out.write(suffix);
                     break;
                 }
-                if write_prefix {
+                if is_start_of_line {
                     out.write(prefix);
-                    write_prefix = false;
+                    is_start_of_line = false;
                 }
                 let ch = character [0] as char;
-                if ch == delimiter {
-                    out.write(infix);
-                } else if ch == '\n' {
-                    out.write(suffix);
-                    prefix = "<tr><td>";
-                    infix = "</td><td>";
-                    suffix = "</td></tr>";
-                    write_prefix = true;
-                } else {
-                    out.character (ch);
+		match ch {
+                    '\t' => out.write(infix),
+                    '\n' => {
+                       	out.write(suffix);
+                    	prefix = "<tr><td>";
+                    	infix  = "</td><td>";
+                    	suffix = "</td></tr>";
+                    	is_start_of_line = true;
+		    },
+                    _ => out.character (ch)
                 }
             },
             Err(message) => { eprintln!("Error {}", message); break; }
         }
     }
-    out.tag("/table");
-    out.tag("/body");
+    out.end_tag("table");
+    out.end_tag("body");
 }
 
-struct HTML {
+///
+/// This class provides some helper methods for writing HTML.
+///
+struct HTMLWriter {}
 
-}
-
-impl HTML {
+impl HTMLWriter {
 
     fn character (&self, ch : char) {
         self.write(ch.to_string().trim());  //FIXME
     }
-    fn tag(&self, text: &str) {
+    fn tag(&self, tag: &str) {
         self.character ('<');
-        self.write  (text);
+        self.write  (tag);
         self.character ('>');
+    }
+    fn end_tag(&self, tag: &str) {
+        self.write ("</");
+        self.write  (tag);
+        self.character ('>');
+    }
+    fn element(&self, tag : &str, text : &str) {
+        self.tag     (tag);
+        self.write   (text);
+        self.end_tag (tag);
     }
     fn write (&self, text : &str)
     {
